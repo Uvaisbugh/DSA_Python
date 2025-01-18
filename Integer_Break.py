18f34bee09daeb80ec1eb71f5800c2851face4fa
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-18 22:57:46 +0530

    Update Integer_Break.py

--- Integer_Break.py
+++ Integer_Break.py
@@ -1,1 +1,20 @@
 # https://leetcode.com/problems/integer-break/
+
+
+# Method 1 ------  Memoization  -------
+# https://youtu.be/in6QbUPMJ3I
+class Solution:
+    def integerBreak(self, n):
+        dp = {1 : 1}
+        
+        def dfs(num):
+            if num in dp:
+                return dp[num]
+            
+            dp[num] = 0 if num == n else num
+            for i in range(1, num):
+                val = dfs(i) * dfs(num - i)
+                dp[num] = max(dp[num], val)
+            return dp[num]
+        
+        return dfs(n)

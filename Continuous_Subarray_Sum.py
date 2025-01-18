6b66a860759bbf8eba4bd27bd44f041df9c9057a
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-18 22:52:02 +0530

    Update Continuous_Subarray_Sum.py

--- Continuous_Subarray_Sum.py
+++ Continuous_Subarray_Sum.py
@@ -2,6 +2,18 @@
 
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        """
+        Determines if the array contains a continuous subarray of at least size 2 
+        whose elements sum up to a multiple of the given integer k.
+
+        Args:
+        nums (List[int]): A list of integers.
+        k (int): An integer to check the multiple of.
+
+        Returns:
+        bool: True if such a subarray exists, False otherwise.
+        """
+
         sumIndexDic = {0 : -1}    # nums = [14, 7]; k = 7
         curSum = 0
         

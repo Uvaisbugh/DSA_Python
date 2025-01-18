1c138cead5efebf55bae9cb59801457fe058aa15
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-18 22:58:46 +0530

    Update Integer_Break.py

--- Integer_Break.py
+++ Integer_Break.py
@@ -5,6 +5,18 @@
 # https://youtu.be/in6QbUPMJ3I
 class Solution:
     def integerBreak(self, n):
+        """
+        Breaks an integer n into the sum of at least two positive integers, aiming to 
+        maximize the product of those integers. Uses a memoization approach to store 
+        intermediate results and avoid redundant computations.
+
+        Args:
+        n (int): The integer to be broken into a sum of at least two positive integers.
+
+        Returns:
+        int: The maximum product achievable by breaking the integer n.
+        """
+
         dp = {1 : 1}
         
         def dfs(num):

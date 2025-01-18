857b0d472aa81e9c36d2d2d5a731a8770cafd9cd
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-18 22:59:20 +0530

    Update Integer_Break.py

--- Integer_Break.py
+++ Integer_Break.py
@@ -30,3 +30,20 @@ class Solution:
             return dp[num]
         
         return dfs(n)
+
+# Method 2 -------  Dynamic Programming -------
+class Solution(object):
+    def integerBreak(self, n):
+        dp = [None, 1]
+        for m in range (2, n + 1):
+            j = m - 1
+            i = 1
+            max_product = 0
+            while i <= j:
+                max_product = max(max_product, max(i, dp[i]) * max(j, dp[j]))
+                j -= 1
+                i += 1
+            dp.append(max_product)
+        return dp[n]
+# Time: O(n^2)
+# Space: O(n)

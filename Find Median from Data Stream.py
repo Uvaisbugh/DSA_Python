26ac588ca56711f0b36fec1370a366b11d922519
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-19 22:51:05 +0530

    Update Find Median from Data Stream.py

---
+++ Find Median from Data Stream.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-median-from-data-stream/
+# https://youtu.be/itmhHWaHupI
+
+# we can easyly solve using arr operations in O(N) time but to solve in log(N) time we have to use heap
+# using 2 heaps one is left side of median ig. self.small(maxheap) and another is right side of median self.large(minheap)
+

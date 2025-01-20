1ac5174a7ae47f8f10c3e953802f9f46092884df
Uvais Bughari <suvaisbughari@gmail.com>
2025-01-20 22:08:01 +0530

    Create Palindrome Pairs.py

---
+++ Palindrome Pairs.py
@@ -0,0 +1,17 @@
+#Palindrome Pairs
+from typing import List
+
+
+class Solution1:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        d = {w: i for i, w in enumerate(words)}
+        ans = []
+        for i, w in enumerate(words):
+            for j in range(len(w) + 1):
+                a, b = w[:j], w[j:]
+                ra, rb = a[::-1], b[::-1]
+                if ra in d and d[ra] != i and b == rb:
+                    ans.append([i, d[ra]])
+                if j and rb in d and d[rb] != i and a == ra:
+                    ans.append([d[rb], i])
+        return ans
